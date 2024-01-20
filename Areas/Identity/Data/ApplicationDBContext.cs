a2adea3b1ca89611aa23e49e682bedfbebe319cc
AneeqUllahKhan <aneequllah.khan@outlook.com>
2024-01-20 17:10:37 +0500

    user-session

---
+++ Areas/Identity/Data/ApplicationDBContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TodoApp.Areas.Identity.Data;
+
+public class ApplicationDBContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(u=> u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
+
+    }
+}
